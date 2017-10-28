23e963b5174a75804c81eef674f45791307eddec
Varun Chadha <vchadha023@gmail.com>
2017-10-27 21:32:42 -0500

    Updated test branch to run simple display

--- TestJavaGame1/src/com/base/game/Main.java
+++ TestJavaGame1/src/com/base/game/Main.java
@@ -1,7 +1,49 @@
 package com.base.game;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.lwjgl.opengl.GL11.*;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("sweet");
+        initDisplay();
+        game();
+        clean();
+    }
+
+    private static void initDisplay() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+
+            Keyboard.create();
+        } catch (LWJGLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void clean() {
+        Display.destroy();
+        Keyboard.destroy();
+    }
+
+    private static void game() {
+        while (!Display.isCloseRequested()) {
+            render();
+        }
+    }
+
+    private static void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+
+        Display.update();
+        Display.sync(60);
     }
 }
